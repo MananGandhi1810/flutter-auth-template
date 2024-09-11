7c72b5e6f2fe99f83c6db915c0cdd01d5568c7d6
Manan Gandhi <ardumanan@gmail.com>
2024-09-11 23:25:13 +0530

    Refactor: Home Page -> Home

--- lib/presentation/main/home_page.dart
+++ lib/presentation/main/home_page.dart
@@ -15,7 +15,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home Page"),
+        title: const Text("Home"),
         actions: [
           IconButton(
             onPressed: () async {
